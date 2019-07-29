80ca83963c264434314b17233943cd0bee45404d
yhocn107 <yho111111>
2019-07-29 17:31:10 +0800

    7.29

---
+++ miniprogram/pages/shouye/shouye.js
@@ -0,0 +1,79 @@
+// pages/shouye/shouye.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     all : []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    var that=this
+    
+    const db = wx.cloud.database();
+    db.collection('YH-school-mod').get({
+
+      success: function (res) {
+
+        console.log(res.data)
+          that.setData({
+          all:res.data
+                      })
+      }
+
+    }) 
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
